a23252302b73e84aa3b4d96b24e9c5383d38ee72
Eric Kuha <erickuha@gmail.com>
2016-03-04 15:58:51 -0600

    Tweaks

--- test/token_test.py
+++ test/token_test.py
@@ -2,6 +2,6 @@ from rest_client import rest_api as rest
 
 
 def get_token(username, password):
-    token = rest.get_auth_token(username, password)
-    #token = rest.get_auth_token('gnome', 'applepie')
+    # token = rest.get_auth_token(username, password)
+    token = rest.get_auth_token('gnome', 'applepie')
     return token.json()['token']
