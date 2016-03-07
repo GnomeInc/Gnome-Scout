b51c03f5be1fb7d538f39b9ca243b5f2e665c3b6
Eric Kuha <erickuha@gmail.com>
2016-03-07 14:41:39 -0600

    automated test_data adding

--- test/post_test.py
+++ test/post_test.py
@@ -2,6 +2,7 @@ from rest_client import rest_api as rest, models, rest_errors
 from test.token_test import get_token
 import datetime
 import random
+import time
 
 
 def make_data(gnome):
@@ -18,29 +19,27 @@ def make_data(gnome):
     }
 
 
-# token = get_token('gnome', '8af3b8180c860161e485479c8b26f353')
-token = get_token('gnome', 'applepie')
-data = make_data(1)
+def do_post():
+    # token = get_token('gnome', '8af3b8180c860161e485479c8b26f353')
+    token = get_token('gnome', 'applepie')
+    data = make_data(1)
+    ds = models.DataSet(data['id'],
+                        data['gnome'],
+                        data['date'],
+                        data['time'],
+                        data['temperature'],
+                        data['humidity'],
+                        data['light_level'],
+                        data['soil_moisture'],
+                        data['soil_nutrients'])
+    ds_dict = ds.__dict__
+    resp = rest.add_data(token, ds_dict)
+    if resp.status_code != 201:
+        raise rest_errors.ApiError(resp.status_code, "Unable to post data")
+    print(resp.json())
 
-ds = models.DataSet(data['id'],
-                    data['gnome'],
-                    data['date'],
-                    data['time'],
-                    data['temperature'],
-                    data['humidity'],
-                    data['light_level'],
-                    data['soil_moisture'],
-                    data['soil_nutrients'])
 
-ds_dict = ds.__dict__
+while True:
+    do_post()
+    time.sleep(60*5)
 
-resp = rest.add_data(token, ds_dict)
-
-if resp.status_code != 201:
-    raise rest_errors.ApiError(resp.status_code, "Unable to post data")
-
-print(resp.json())
-
-mod = models.make_data_set(resp.json())
-
-print(mod.light_level)
